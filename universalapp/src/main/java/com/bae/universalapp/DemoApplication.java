ee5baa9e3c428d7bc75ce83eeb10ed51e0e1a018
Harr1s96 <hanleyharris96@gmail.com>
2019-12-11 12:51:07 +0000

    changed package structure

--- universalapp/src/main/java/com/bae/universalapp/DemoApplication.java
+++ universalapp/src/main/java/com/bae/universalapp/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.bae.universalapp.main;
+package com.bae.universalapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ universalapp/src/main/java/com/bae/universalapp/repo/User.java
@@ -0,0 +1,12 @@
+package com.bae.universalapp.repo;
+
+import javax.persistence.Entity;
+
+/**
+ * User
+ */
+@Entity
+ public class User {
+
+    
+}
