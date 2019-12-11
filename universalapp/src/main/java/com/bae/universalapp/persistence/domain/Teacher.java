5295a6aec59850d6513ab0f065e87a29b61822d2
Harr1s96 <hanleyharris96@gmail.com>
2019-12-11 22:38:41 +0000

    added teacher Spring stack

--- universalapp/src/main/java/com/bae/universalapp/persistence/domain/Teacher.java
+++ universalapp/src/main/java/com/bae/universalapp/persistence/domain/Teacher.java
@@ -1,12 +1,64 @@
-// package com.bae.universalapp.persistence.domain;
+package com.bae.universalapp.persistence.domain;
 
-// //import javax.persistence.Entity;
+import javax.persistence.*;
 
-// /**
-//  * Lecturer
-//  */
-// //@Entity
-// public class Teacher {
+/**
+ * Lecturer
+ */
+@Entity
+public class Teacher {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+    private String lastName;
+
+    public Teacher() {
+
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
 
     
-// }
+}
